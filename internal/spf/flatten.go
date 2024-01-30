6fc5c7fa30ce7cbec29b973b3617466b95b70cc2
Lena Underwood <lena@letsencrypt.org>
2024-01-30 13:24:45 -0500

    split files; flags struct; NewRootSPF(); exit code

---
+++ internal/spf/flatten.go
@@ -0,0 +1,175 @@
+// FLATTEN SPF RECORD =========================================================================
+package spf
+
+import (
+	"fmt"
+	"log/slog"
+	"net"
+	"regexp"
+	"strings"
+)
+
+type Lookup interface {
+	LookupTXT(string) ([]string, error)
+	LookupIP(string) ([]net.IP, error)
+	LookupMX(string) ([]*net.MX, error)
+}
+
+type NetLookup struct{}
+
+func (n NetLookup) LookupTXT(s string) ([]string, error) {
+	return net.LookupTXT(s)
+}
+func (n NetLookup) LookupIP(s string) ([]net.IP, error) {
+	return net.LookupIP(s)
+}
+func (n NetLookup) LookupMX(s string) ([]*net.MX, error) {
+	return net.LookupMX(s)
+}
+
+func writeIPMech(ip net.IP, prefix string) string {
+	IPV := map[int]string{
+		4:  "4",
+		16: "6",
+	}
+	return "ip" + IPV[len(ip)] + ":" + ip.String() + prefix
+}
+
+type RootSPF struct {
+	RootDomain   string
+	AllMechanism string
+	MapIPs       map[string]bool
+	MapNonflat   map[string]bool
+	LookupIF     Lookup
+}
+
+func NewRootSPF(rootDomain string, lookupIF Lookup) RootSPF {
+	return RootSPF{RootDomain: rootDomain, LookupIF: lookupIF, MapIPs: map[string]bool{}, MapNonflat: map[string]bool{}}
+}
+
+// Check or lookup SPF record for domain, then parse each mechanism.
+// This runs recursively until every mechanism is added either to
+// r.AllMechanism, r.MapIPs, or r.MapNonflat (or ignored)
+func (r *RootSPF) FlattenSPF(domain, spfRecord string) error {
+	slog.Debug("--- Flattening domain ---", "domain", domain)
+	spfRecord, err := CheckSPFRecord(domain, spfRecord, r.LookupIF)
+	if err != nil {
+		return fmt.Errorf("invalid SPF record for %s:\n %s", domain, err)
+	}
+	containsAll := regexp.MustCompile(`^.* (\+|-|~|\?)?all$`).MatchString(spfRecord)
+	for _, mechanism := range strings.Split(spfRecord, " ")[1:] {
+		// If not `all`, skip mechanism if fail modifier (- or ~) and ignore modifier otherwise
+		if regexp.MustCompile(`^(\+|-|~|\?)$`).MatchString(mechanism[:1]) && !regexp.MustCompile(`^all$`).MatchString(mechanism[1:]) {
+			if regexp.MustCompile(`^(-|~)$`).MatchString(mechanism[:1]) {
+				continue
+			}
+			mechanism = mechanism[1:]
+		}
+		// Skip `redirect` if SPF record includes an `all` mechanism
+		isRedirect := regexp.MustCompile(`^redirect=.*$`).MatchString(mechanism)
+		if isRedirect && containsAll {
+			continue
+		}
+		// Parse mechanism
+		err := r.ParseMechanism(strings.TrimSpace(mechanism), domain)
+		if err != nil {
+			return fmt.Errorf("could not flatten SPF record for %s:\n %s", domain, err)
+		}
+		// Skip all mechanisms after `redirect`
+		if isRedirect {
+			break
+		}
+	}
+	return nil
+}
+
+// Parse the given mechanism and dispatch it accordingly
+func (r *RootSPF) ParseMechanism(mechanism, domain string) error {
+	lastSlashIndex := strings.LastIndex(mechanism, "/")
+	switch {
+	// Copy `all` mechanism if set by ROOT_DOMAIN
+	case regexp.MustCompile(`^(\+|-|~|\?)?all`).MatchString(mechanism):
+		if domain == r.RootDomain {
+			slog.Debug("Setting `all` mechanism", "mechanism", mechanism)
+			r.AllMechanism = " " + mechanism
+		}
+	// Add IPv4 and IPv6 addresses to r.MapIPs
+	case regexp.MustCompile(`^ip(4|6):.*$`).MatchString(mechanism):
+		slog.Debug("Adding IP mechanism", "mechanism", mechanism)
+		r.MapIPs[mechanism] = true
+	// Convert A/AAAA and MX records, then add to r.MapIPs
+	case regexp.MustCompile(`^a$`).MatchString(mechanism): // a
+		return r.ConvertDomainToIP(domain, "")
+	case regexp.MustCompile(`^a/\d{1,3}`).MatchString(mechanism): // a/<prefix-length>
+		return r.ConvertDomainToIP(domain, mechanism[1:])
+	case regexp.MustCompile(`^a:.*/\d{1,3}$`).MatchString(mechanism): // a:<domain>/<prefix-length>
+		return r.ConvertDomainToIP(mechanism[strings.Index(mechanism, ":")+1:lastSlashIndex], mechanism[lastSlashIndex:])
+	case regexp.MustCompile(`^a:.*$`).MatchString(mechanism): // a:<domain>
+		return r.ConvertDomainToIP(strings.SplitN(mechanism, ":", 2)[1], "")
+	case regexp.MustCompile(`^mx$`).MatchString(mechanism): // mx
+		return r.ConvertMxToIP(domain, "")
+	case regexp.MustCompile(`^mx/\d{1,3}`).MatchString(mechanism): // mx/<prefix-length>
+		return r.ConvertMxToIP(domain, mechanism[2:])
+	case regexp.MustCompile(`^mx:.*/\d{1,3}$`).MatchString(mechanism): // mx:<domain>/<prefix-length>
+		return r.ConvertMxToIP(mechanism[strings.Index(mechanism, ":")+1:lastSlashIndex], mechanism[lastSlashIndex:])
+	case regexp.MustCompile(`mx:.*$`).MatchString(mechanism): // mx:<domain>
+		return r.ConvertMxToIP(strings.SplitN(mechanism, ":", 2)[1], "")
+	// Add ptr, exists, and exp mechanisms to r.MapNonflat
+	case regexp.MustCompile(`^ptr$`).MatchString(mechanism):
+		slog.Debug("Adding nonflat mechanism", "mechanism", mechanism+":"+domain)
+		r.MapNonflat[mechanism+":"+domain] = true
+	case regexp.MustCompile(`^(ptr:|exists:|exp=).*$`).MatchString(mechanism):
+		slog.Debug("Adding nonflat mechanism", "mechanism", mechanism)
+		r.MapNonflat[mechanism] = true
+	// Recursive call to FlattenSPF on `include` and `redirect` mechanism
+	case regexp.MustCompile(`^(include:|redirect=).*$`).MatchString(mechanism):
+		return r.FlattenSPF(mechanism[strings.IndexAny(mechanism, ":=")+1:], "")
+	// Return error if no match
+	default:
+		return fmt.Errorf("received unexpected SPF mechanism or syntax: '%s'", mechanism)
+	}
+	return nil
+}
+
+// Convert A/AAAA records to IPs and add them to r.MapIPs
+func (r *RootSPF) ConvertDomainToIP(domain, prefixLength string) error {
+	slog.Debug("Looking up IP records for domain", "domain", domain)
+	ips, err := r.LookupIF.LookupIP(domain)
+	if err != nil {
+		return fmt.Errorf("could not lookup IPs for %s:\n %s", domain, err)
+	}
+	for _, ip := range ips {
+		slog.Debug("Adding IP mechanism", "mechanism", writeIPMech(ip, prefixLength))
+		r.MapIPs[writeIPMech(ip, prefixLength)] = true
+	}
+	return nil
+}
+
+// Convert MX records to domains then to IPs and add them to r.MapIPs
+func (r *RootSPF) ConvertMxToIP(domain, prefixLength string) error {
+	slog.Debug("Looking up MX records for domain", "domain", domain)
+	mxs, err := r.LookupIF.LookupMX(domain)
+	if err != nil {
+		return fmt.Errorf("could not lookup MX records for %s:\n %s", domain, err)
+	}
+	for _, mx := range mxs {
+		slog.Debug("Found MX record for domain", "mx_record", mx.Host)
+		if err := r.ConvertDomainToIP(mx.Host, prefixLength); err != nil {
+			return fmt.Errorf("could not lookup IPs for MX record `%s`:\n %s", mx.Host, err)
+		}
+	}
+	return nil
+}
+
+// Flatten and write new SPF record for root domain by compiling r.AllMechanism, r.MapIPs, and r.MapNonflat
+func (r *RootSPF) WriteFlatSPF() string {
+	flatSPF := "v=spf1"
+	for ip := range r.MapIPs {
+		flatSPF += " " + ip
+	}
+	for nonflat := range r.MapNonflat {
+		flatSPF += " " + nonflat
+	}
+	flatSPF += r.AllMechanism
+	return flatSPF
+}
--- internal/spf/record_test.go
+++ internal/spf/record_test.go
@@ -1,295 +1,30 @@
 package spf
 
 import (
-	"fmt"
-	"net"
 	"regexp"
 	"testing"
 )
 
-// MOCK DATA & HELPER FUNCTIONS ===============================================================
-
-type mockLookup struct{}
-
-func (n mockLookup) LookupTXT(s string) ([]string, error) {
-	return TXT_LOOKUP[s], nil
-}
-func (n mockLookup) LookupIP(s string) ([]net.IP, error) {
-	return IP_LOOKUP[s], nil
-}
-func (n mockLookup) LookupMX(s string) ([]*net.MX, error) {
-	return MX_LOOKUP[s], nil
-}
-
-var TXT_LOOKUP = map[string][]string{
-	"mydomain":    {"NOT AN SPF RECORD", "v=spf1 a ~all"},
-	"test.com":    {"v=spf1 ip4:10.10.10.10", "exp='IDK why that failed, good luck'"},
-	"example.com": {"v=spf1 include:mydomain exp=exp.example.com"},
-	"nospf":       {"NOT AN SPF RECORD", "v=also not an spf record"},
-}
-
-var IP_LOOKUP = map[string][]net.IP{
-	"mydomain":                               {net.IP{0, 0, 0, 0}, net.IP{1, 2, 3, 4}},
-	"example.com":                            {net.ParseIP("2001:db8::68")},
-	"abcd.net":                               {net.IP{12, 34, 56, 78}, net.ParseIP("2001:db8::1"), net.IP{6, 6, 6, 6}},
-	"info.info":                              {net.IP{1, 1, 1, 1}, net.IP{2, 2, 2, 2}, net.IP{3, 3, 3, 3}, net.ParseIP("1:2:3:4:5:6:7:8")},
-	"longer.domain/subdomain/subsubdomain/a": {net.ParseIP("1234:34::1")},
-}
-var MX_LOOKUP = map[string][]*net.MX{
-	"anotherdomain": {&net.MX{Host: "mydomain", Pref: 10}},
-	"test.org":      {&net.MX{Host: "example.com", Pref: 20}, &net.MX{Host: "abcd.net", Pref: 10}},
-	"test.domain":   {&net.MX{Host: "example.com", Pref: 10}, &net.MX{Host: "info.info", Pref: 30}},
-}
-
-// Check if input error is nil, if r.ALlMechanism is expected, and if r.MapIPs and r.MapNonflat have the expected entries.
-func (r *RootSPF) compareExpected(err error, expAll string, expIPs, expNF []string) error {
-	if err != nil {
-		return fmt.Errorf("unexpected error:\n %s", err)
-	}
-	if r.AllMechanism != expAll {
-		return fmt.Errorf("expected `%s` for r.AllMechanism but got `%s`", expAll, r.AllMechanism)
-	}
-	for _, expIP := range expIPs {
-		if _, ok := r.MapIPs[expIP]; !ok {
-			return fmt.Errorf("expected `%s` in r.MapIPs but not found", expIP)
-		}
-	}
-	for _, expNF := range expNF {
-		if _, ok := r.MapNonflat[expNF]; !ok {
-			return fmt.Errorf("expected `%s` in r.MapNonflat but not found", expNF)
-		}
-	}
-	return nil
-}
-
-// TESTS FOR parseMechanism() =================================================================
-
-func TestParseMechanismAll(t *testing.T) {
-	r := RootSPF{RootDomain: "myrootdomain", LookupIF: mockLookup{}}
-	// Test `all`` mechanism set if domain is root
-	err := r.ParseMechanism("~all", "myrootdomain")
-	if err = r.compareExpected(err, " ~all", []string{}, []string{}); err != nil {
-		t.Fatal(err)
-	}
-	// Test `all`` mechanism is ignored if domain is NOT root
-	err = r.ParseMechanism("-all", "NOTmyrootdomain")
-	if err = r.compareExpected(err, " ~all", []string{}, []string{}); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestParseMechanismIP(t *testing.T) {
-	r := RootSPF{MapIPs: map[string]bool{}, LookupIF: mockLookup{}}
-	// Test ip mechanisms of the form `ip4:<ipaddr>`, `ip4:<ipaddr>/<prefix-length>, `ip6:<ipaddr>`, and `ip6:<ipaddr>/<prefix-length>`
-	ipMechs := []string{"ip4:abcd", "ip4:8.8.8.8", "ip6:efgh/36", "ip6:2001:0db8:85a3:0000:0000:8a2e:0370:7334", "ip6:11:22::33/128"}
-	for _, mech := range ipMechs {
-		err := r.ParseMechanism(mech, "")
-		if err = r.compareExpected(err, "", []string{mech}, []string{}); err != nil {
-			t.Fatal(err)
-		}
-	}
-}
-
-func TestParseMechanismA(t *testing.T) {
-	r := RootSPF{LookupIF: mockLookup{}}
-	// Test `a` mechanism of the form `a`, `a:<domain>`, `a/<prefix-length>`, and `a:<domain>/<prefix-length`
-	testInputs := [][]string{ //mechanism, prefix, aDomain, currentDomain
-		{"a", "", "mydomain", "mydomain"},
-		{"a:abcd.net", "", "abcd.net", "differentdomain"},
-		{"a/24", "/24", "longer.domain/subdomain/subsubdomain/a", "longer.domain/subdomain/subsubdomain/a"},
-		{"a:info.info/46", "/46", "info.info", "differentdomain"},
-	}
-	for _, testCase := range testInputs {
-		r.MapIPs = map[string]bool{}
-		expIPs := []string{}
-		for _, ip := range IP_LOOKUP[testCase[2]] {
-			expIPs = append(expIPs, writeIPMech(ip, testCase[1]))
-		}
-		err := r.ParseMechanism(testCase[0], testCase[3])
-		if err = r.compareExpected(err, "", expIPs, []string{}); err != nil {
-			t.Fatal(err)
-		}
-	}
-}
-
-func TestParseMechanismMX(t *testing.T) {
-	r := RootSPF{LookupIF: mockLookup{}}
-	// Test `mx` mechanism of the form `mx`, `mx:<domain>`, `mx/<prefix-length>`, and `mx:<domain>/<prefix-length`
-	testInputs := [][]string{ // mechanism, prefix, mxDomain, currentDomain
-		{"mx", "", "anotherdomain", "anotherdomain"},
-		{"mx:test.org", "", "test.org", "differentdomain"},
-		{"mx/19", "/19", "test.domain", "test.domain"},
-		{"mx:anotherdomain/6", "/6", "anotherdomain", "differentdomain"},
-	}
-	for _, testCase := range testInputs {
-		r.MapIPs = map[string]bool{}
-		expIPs := []string{}
-		for _, d := range MX_LOOKUP[testCase[2]] {
-			for _, ip := range IP_LOOKUP[d.Host] {
-				expIPs = append(expIPs, writeIPMech(ip, testCase[1]))
-			}
-		}
-		err := r.ParseMechanism(testCase[0], testCase[3])
-		if err = r.compareExpected(err, "", expIPs, []string{}); err != nil {
-			t.Fatal(err)
-		}
-	}
-}
-
-func TestParseMechanismNonFlat(t *testing.T) {
-	r := RootSPF{MapNonflat: map[string]bool{}, LookupIF: mockLookup{}}
-	// Test ptr mechanism of the form `ptr`
-	err := r.ParseMechanism("ptr", "domain")
-	if err = r.compareExpected(err, "", []string{}, []string{"ptr:domain"}); err != nil {
-		t.Fatal(err)
-	}
-	// Test nonflat mechanisms of the form `ptr:<domain>`, `<exists:<domain>`, and `exp=<domain>`
-	nfMechs := []string{"ptr:example.com", "exists:yourdomain", "exp=explain.example.com"}
-	for _, nfMech := range nfMechs {
-		err := r.ParseMechanism(nfMech, "")
-		if err = r.compareExpected(err, "", []string{}, []string{nfMech}); err != nil {
-			t.Fatal(err)
-		}
-	}
-}
-
-func TestParseMechanismInclude(t *testing.T) {
-	r := RootSPF{MapIPs: map[string]bool{}, LookupIF: mockLookup{}}
-	// Test mechanism of the form `include:<domain>`
-	includeDomain := "mydomain" // SPF record is just `a`, so expect IPs for "mydomain"
-	expIPs := []string{}
-	for _, ip := range IP_LOOKUP[includeDomain] {
-		expIPs = append(expIPs, writeIPMech(ip, ""))
-	}
-	err := r.ParseMechanism("include:"+includeDomain, "notmydomain")
-	if err = r.compareExpected(err, "", expIPs, []string{}); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestParseMechanismRedirect(t *testing.T) {
-	r := RootSPF{MapIPs: map[string]bool{}, LookupIF: mockLookup{}}
-	// Test mechanism of the form `redirect=<domain>`
-	redirectDomain := "test.com" // SPF record is just ip4:10.10.10.10
-	err := r.ParseMechanism("redirect="+redirectDomain, "notmydomain")
-	if err = r.compareExpected(err, "", []string{"ip4:10.10.10.10"}, []string{}); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestParseMechanismFails(t *testing.T) {
-	r := RootSPF{LookupIF: mockLookup{}}
-	// Test that parseMechanism fails on unexpected mechanism or syntax error
-	noMatchRegex := regexp.MustCompile("^(received unexpected SPF mechanism or syntax:).*$")
-	for _, wrongMech := range []string{"redirect:domain", "include=anotherdomain", "ip:0.0.0.0", "1.1.1.1", "", "ip6", "exp:explanation", "notMechanism:hello"} {
-		err := r.ParseMechanism(wrongMech, "")
-		if !noMatchRegex.MatchString(err.Error()) {
-			t.Fatalf("Expected `unexpected SPF mechanism or syntax` error, got `%s` instead", err)
-		}
-	}
-}
-
-// TESTS FOR FlattenSPF() =====================================================================
-
-func TestFlattenSPF(t *testing.T) {
-	r := RootSPF{MapIPs: map[string]bool{}, MapNonflat: map[string]bool{}, LookupIF: mockLookup{}}
-	// Test that SPF record with multiple different entries gets flattened as expected
-	domain, spf := "abcd.net", "v=spf1 ip4:0.0.0.0/24 include:example.com ip6:56:0::0:7 a mx:test.domain exists:somedomain.edu -all"
-	// include:example.com --> (mydomain spf --> a --> mydomain IPs) + exp=exp.example.com
-	// a --> abcd.net IPs
-	// mx:test.domain --> example.com IPs and info.info IPs
-	r.RootDomain = domain
-	expIPs := []string{"ip4:0.0.0.0/24", "ip6:56:0::0:7"}
-	expNFs := []string{"exists:somedomain.edu", "exp=exp.example.com"}
-	for _, d := range []string{"mydomain", "abcd.net", "info.info", "example.com"} {
-		for _, ip := range IP_LOOKUP[d] {
-			expIPs = append(expIPs, writeIPMech(ip, ""))
-		}
-	}
-	err := r.FlattenSPF(domain, spf)
-	if err = r.compareExpected(err, " -all", expIPs, expNFs); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestFlattenRedirects(t *testing.T) {
-	r := RootSPF{MapIPs: map[string]bool{}, LookupIF: mockLookup{}}
-	domain, spf := "somedomain", "v=spf1 ip4:9.9.9.9 redirect=mydomain ip6:2:2:2:2::::"
-	// Test that mechanisms after redirects are ignored
-	expIPs := []string{"ip4:9.9.9.9"}
-	for _, ip := range IP_LOOKUP["mydomain"] {
-		expIPs = append(expIPs, writeIPMech(ip, ""))
-	}
-	err := r.FlattenSPF(domain, spf)
-	if err = r.compareExpected(err, "", expIPs, []string{}); err != nil {
-		t.Fatal(err)
-	}
-	if _, ok := r.MapIPs["ip6:2:2:2:2::::"]; ok {
-		t.Fatal("Mechanisms after redirect should have been ignored")
-	}
-	// Test that redirects are ignored if SPF record includes `all` mechanism
-	r.MapIPs = map[string]bool{}
-	err = r.FlattenSPF(domain, spf+" ~all")
-	if err = r.compareExpected(err, "", []string{"ip4:9.9.9.9"}, []string{}); err != nil {
-		t.Fatal(err)
-	}
-	if len(r.MapIPs) > 2 {
-		t.Fatal("Redirect should have been ignored since `all` mechanism in SPF record")
-	}
-}
-
-func TestFlattenModifiers(t *testing.T) {
-	// Test modifier logic
-	r := RootSPF{MapIPs: map[string]bool{}, MapNonflat: map[string]bool{}, LookupIF: mockLookup{}}
-	domain, spf := "somedomain", "v=spf1 -include:mydomain +ip4:9.8.7.6/54 ~exists:otherdomain ?mx:test.domain -all"
-	r.RootDomain = domain
-	expIPs := []string{"ip4:9.8.7.6/54"}
-	for _, d := range MX_LOOKUP["test.domain"] {
-		for _, ip := range IP_LOOKUP[d.Host] {
-			expIPs = append(expIPs, writeIPMech(ip, ""))
-		}
-	}
-	err := r.FlattenSPF(domain, spf)
-	if err = r.compareExpected(err, " -all", expIPs, []string{}); err != nil {
-		t.Fatal(err)
-	}
-	// Test IPs for `mydomain` are skipped because of fail modifier - on `include` mechanism
-	for _, ip := range IP_LOOKUP["mydomain"] {
-		if _, ok := r.MapIPs[writeIPMech(ip, "")]; ok {
-			t.Fatal("IP should have been ignored because its include modifier was `-`")
-		}
-		expIPs = append(expIPs, "-"+writeIPMech(ip, ""))
-	}
-	// Test `exists` mechanism skipped because of its fail modifier ~
-	if _, ok := r.MapNonflat["exists:otherdomain"]; ok {
-		t.Fatal("`exists` should have been ignored because its modifier was `~`")
-	}
-}
-
-// TESTS FOR GetDomainSPF() ===================================================================
-
 func TestGetDomainSPF(t *testing.T) {
-	r := RootSPF{LookupIF: mockLookup{}}
+	r := NewRootSPF("", mockLookup{})
 	// Check that correctly filters TXT records for SPF record
-	record, _ := r.GetDomainSPFRecord("mydomain")
+	record, _ := GetDomainSPFRecord("mydomain", r.LookupIF)
 	if record != "v=spf1 a ~all" {
 		t.Fatal("Filtering for SPF record failed.")
 	}
 	// Check that returns error when no SPF record found
-	_, err := r.GetDomainSPFRecord("nospf")
+	_, err := GetDomainSPFRecord("nospf", r.LookupIF)
 	if !regexp.MustCompile("^no SPF record found for.*$").MatchString(err.Error()) {
 		t.Fatal("Should have failed to find SPF record for `nospf`")
 	}
 }
 
-// TESTS FOR CheckDomainSPF() =================================================================
-
 func TestCheckDomainSPF(t *testing.T) {
-	r := RootSPF{LookupIF: mockLookup{}}
+	r := NewRootSPF("", mockLookup{})
 	domain, record := "mydomain", "v=spf1 a ~all"
 	// Check that SPF record matches expected whether or not provided in input
 	for _, inputSPF := range []string{"", record} {
-		checkedRecord, err := r.CheckSPFRecord(domain, inputSPF)
+		checkedRecord, err := CheckSPFRecord(domain, inputSPF, r.LookupIF)
 		if checkedRecord != record {
 			t.Fatalf("SPF record lookup did not match expected. Expected `%s` but got `%s`", record, checkedRecord)
 		}
@@ -298,34 +33,11 @@ func TestCheckDomainSPF(t *testing.T) {
 		}
 	}
 	// Check that returns error when SPF record does not match expected format
-	_, err := r.CheckSPFRecord(domain, "spf1 a ~all")
+	_, err := CheckSPFRecord(domain, "spf1 a ~all", r.LookupIF)
 	if !regexp.MustCompile("^.*did not match expected format.*$").MatchString(err.Error()) {
 		t.Fatal("SPF record check should have failed")
 	}
 }
-
-// TESTS FOR WriteFlatSPF() ===================================================================
-
-func TestWriteFlatSPF(t *testing.T) {
-	r := RootSPF{
-		AllMechanism: " -all",
-		MapIPs:       map[string]bool{"ip4:0.0.0.0": true, "ip6:1:1:1:1::::": true},
-		MapNonflat:   map[string]bool{"exists:domain": true, "ptr": true, "exists:anotherdomain": true},
-	}
-	expectedSPF := "v=spf1 ip4:0.0.0.0 ip6:1:1:1:1:::: exists:domain ptr exists:anotherdomain -all"
-
-	// Check that written SPF record starts with `v=sfp1` and ends with `all` mechanism
-	outputSPF := r.WriteFlatSPF()
-	if regexp.MustCompile("^(v=spf1).* (/-all)$").MatchString(outputSPF) {
-		t.Fatalf("SFP record should start with 'v=sfp1' and end with `all` mechanism if set.\nGot: `%s`", outputSPF)
-	}
-	// Check that both SPF records have the same entries (order doesn't matter)
-	if matched, inStart, inEnd := CompareRecords(expectedSPF, outputSPF); !matched {
-		t.Fatalf("SPF record does not include expected entries. Should include `%s` and should not include `%s`", inStart, inEnd)
-	}
-}
-
-// TESTS FOR CompareRecords() =================================================================
 
 func TestCompareRecords(t *testing.T) {
 	// Check that correctly returns differences between string entries (if any)
